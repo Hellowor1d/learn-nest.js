64e3ba90837f0a40ec854f32f04ca905f7c54e60
Hellowor1d <1034597776@qq.com>
2018-12-18 23:09:07 +0800

    learn the use of @Get decorator , difference between return string and object

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -2,8 +2,18 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+
+    // @Get() 装饰器，把路由接收到的特定请求(GET : '/cats')指向此处理器
     @Get()
     findAll(){
-        return 'This is come from nest cats controller return'
+
+        //返回字符串时，不会自动序列化为 json
+        return 'This is come from nest cats controller'
+
+        //返回对象或者数组，会被自动序列化为 json
+        // return {
+        //     a: 1,
+        //     b: [ 1, 2, 3 ]
+        // }
     }
 }
