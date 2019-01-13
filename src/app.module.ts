451dc43ce382e5cd01ad51170c66c030ab7d068a
1034597776@qq.com <hellovvor1d>
2019-01-13 18:29:37 +0800

    comments for module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 
 @Module({
+  //通过module 把 controller、 service 组装在一起
   imports: [],
   controllers: [AppController, CatsController],
   providers: [AppService],
