b1a93142b4ae0418e09a6235b67548b7ef38ef64
Hellowor1d <1034597776@qq.com>
2019-01-31 00:55:44 +0800

    format & 尝试动态参数未果

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post, Req, Query } from '@nestjs/common';
-import {CreateCatDto} from './create-cat.dto'
+import { Body, Controller, Get, Post, Req, Query, Param } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto'
 @Controller('cats')
 export class CatsController {
 
@@ -7,14 +7,14 @@ export class CatsController {
 
     @Post()
     async create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+        return 'This action adds a new cat';
     }
 
     // @Get() 装饰器，把路由接收到的特定请求(GET : '/cats')指向此处理器
     @Get('ab*cd')
     // @HttpCode(206)
-    findAll(@Query() query){
-            console.log(query)
+    findAll(@Query() query) {
+        console.log(query)
         //返回字符串时，不会自动序列化为 json
         return 'This is come from nest cats controller'
 
@@ -23,5 +23,10 @@ export class CatsController {
         //     a: 1,
         //     b: [ 1, 2, 3 ]
         // }
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return `This action returns a #${id} cat`;
     }
 }
